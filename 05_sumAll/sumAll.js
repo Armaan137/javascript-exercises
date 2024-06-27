b2e3ac82cf943b9b65f39c2defa82afb312bfd81
Armaan <a.khabra5@gmail.com>
2024-06-26 19:16:50 -0600

    Solve exercises up to 07_tempConversion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0
 
+    if (!(typeof start === "number") || !(typeof end === "number")) {
+        return "ERROR"
+    }
+
+    if (start < 0 || end < 0) {
+        return "ERROR"
+    }
+
+    if (end < start) {
+        temp = start
+        start = end
+        end = temp 
+    }
+
+    for (i = start; i <= end; i++) {
+        total += i
+    }
+
+    return total
 };
 
 // Do not edit below this line
