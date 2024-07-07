e0d219b80ecc210d84aeb7fe9761b4ee680a1bcb
Armaan <a.khabra5@gmail.com>
2024-07-06 22:42:22 -0600

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let left = 0
+    let right = word.length - 1
 
+    while (left <= right) {
+        while (!isAlphanumeric(word[left])) {
+            left++
+        }
+
+        while (!isAlphanumeric(word[right])) {
+            right--
+        }
+
+        if (word[left].toLowerCase() !== word[right].toLowerCase()) {
+            return false
+        } 
+
+        left ++ 
+        right--
+    }
+
+    return true
 };
+
+function isAlphanumeric(char) {
+    return /^[a-zA-Z0-9]$/.test(char);
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
