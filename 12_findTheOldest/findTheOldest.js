005c85086eeacb8e96f5f0a822ade552acd22bba
Armaan <a.khabra5@gmail.com>
2024-07-07 00:42:47 -0600

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, curr) => {
+        let ageOne = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        let ageTwo = getAge(curr.yearOfBirth, curr.yearOfDeath)
 
+        return ageTwo >= ageOne ? curr : oldest
+    })
 };
+
+function getAge(birth, death) {
+    const today = new Date().getFullYear()
+
+    if (!death) {
+        return today - birth
+    }
+
+    return death - birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
